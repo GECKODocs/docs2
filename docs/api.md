07c535dd3c84818364e502419603b31fd3c22c6a
GECKODocs <aj@geckogovernance.com>
2021-08-09 12:21:56 +0100

    apis added and sequence of sidebars changed

--- docs/api.md
+++ docs/api.md
@@ -8,4 +8,6 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 
 <img alt="In Maintenance" src={useBaseUrl('img/apiMonitor.svg')}/>
 
-Our API is currently under construction. Please check back at later date for updates.
+Our API is currently authentication-based only, tokens available in your GECKO profile.
+
+Please visit: <a href="https://app.swaggerhub.com/apis/GeckoGovernance/api/0.1"> GECKO API </a> to test them in real time.
--- sidebars.js
+++ sidebars.js
@@ -2,14 +2,15 @@ module.exports = {
   someSidebar: {
     "Getting Started": ["getting_started","next_steps","user_preferences", "system_settings"],
     "Dashboard": ["dashboard"],
-    "Workspaces": ["workspace_overview", "workspace_tasks", "workspace_archiving", "workspace_templates"],
+    "Workspaces": ["workspace_overview", "workspace_tasks", "workspace_templates", "workspace_archiving"],
     "Tasks" : ["tasks"],
     "Reporting" : ["reporting"],
     "Audit" : ["audit"],
     "Calendar" : ["calendar"],
     "File Repository" : ["file_repository"],
-    "Setup": ["setup"]
-    //"Users" : ["users"],
+    "Setup": ["setup"],
+    "API": ["api"]
+    //"Users" : ["users"]
     
   },
   
